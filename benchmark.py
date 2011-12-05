acead8fcfb6aea6c6e66d1b2f465ee81ae964526
Jan Oberst <mail@janoberst.com>
2011-12-05 13:04:18 -0500

    changed benchmark to use time series filter

--- benchmark.py
+++ benchmark.py
@@ -9,13 +9,14 @@ from bloomfilter import BloomFilter, TimeSeriesBloomFilter
 connection = redis.Redis()
 
 filter_size = 8 * 500 * 1024
-test_amount = 10000
+test_amount = 1000 * 100
 
+TimeSeriesBloomFilter
+f = TimeSeriesBloomFilter(connection=connection, bitvector_key='test_bloomfilter', n=filter_size, k=4)
+# f = BloomFilter(connection=connection, bitvector_key='test_bloomfilter', n=filter_size, k=4)
 
-f = BloomFilter(connection=connection, bitvector_key='test_bloomfilter', n=filter_size, k=4)
-
-print "filling bloom filter of %.2fkB size with %ik values" % \
-    (filter_size/1024.0/8, test_amount/1000)
+print "filling bloom filter of %.2fkB size with %.1fk values" % \
+    (filter_size/1024.0/8, test_amount/1000.0)
 
 # create a reference dict so that we can check for false positives
 ref = {}
@@ -36,16 +37,18 @@ print "adding %i values took %.2fs (%i values/sec, %.2f us/value)" % \
 
 # try random values and see how many false positives we'll get
 false_positives = 0
-correct_negatives = 0
-while correct_negatives < test_amount:
+correct_responses = 0
+start = datetime.now()
+while correct_responses < test_amount:
     val = '%.8f' % random.random()
     if (val in f) and (val not in ref):
         false_positives += 1
     else:
-        correct_negatives += 1
-
+        correct_responses += 1
+duration = datetime.now()-start
+print "duration:",duration
 print "correct: %s / false: %s -> %.4f%% false positives" % \
-    (correct_negatives, false_positives, 100*false_positives/float(correct_negatives))
+    (correct_responses, false_positives, 100*false_positives/float(correct_responses))
 
 # remove the key in redis
 connection.delete('test_bloomfilter')
